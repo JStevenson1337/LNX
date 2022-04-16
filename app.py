a44911c7108f61cbdbc7153a66a71a77b6f7fd83
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2022-04-16 01:39:29 -0500

    from: stash

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-import pdb
-from flask import render_template, request, Flask
-from db import save_data
-from models.events import Events
-from dataclasses import fields
-
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,11 +7,13 @@ def home():
     data = {"name":"JI"}
     return render_template("index.html", **data)
 
+
 @app.route("/search/calendar_search")
 def calendar_search():
     data = {}
     return render_template("calendar_search.html", **data)
 
+<<<<<<< Updated upstream
 
 @app.route("/search/calendar_create", methods=["GET", "POST"])
 def calendar_create():
@@ -26,20 +24,12 @@ def calendar_create():
         )
     save_data(request.data)
 
+=======
+>>>>>>> Stashed changes
 @app.route("/search/event_search")
 def event_search():
     data = {}
     return render_template("event_search.html", **data)
-
-
-@app.route("/search/event_create", methods=["GET", "POST"])
-def event_create():
-    if request.method == "GET":
-        return render_template(
-            "event_create.html",
-            fields=Events.fields()
-        )
-    save_data(request.data)
 
 @app.route("/search/event_page")
 def event_page():
@@ -51,11 +41,18 @@ def public_event():
     data = {}
     return render_template("public_event.html", **data)
 
+<<<<<<< Updated upstream
 
 @app.route("/search/private_create")
 def private_create():
     data = {}
     return render_template("private_create.html", **data)
+=======
+@app.route("/search/private_event")
+def private_event():
+    data = {}
+    return render_template("private_event.html", **data)
+>>>>>>> Stashed changes
 
 @app.route("/search/semi_private")
 def semi_private():
@@ -72,20 +69,10 @@ def public_calendar():
     data = {}
     return render_template("public_calendar.html", **data)
 
-@app.route("/search/public_calendar_create")
-def public_calendar_create():
-    data = {}
-    return render_template("public_calendar_create.html", **data)
-
 @app.route("/search/private_calendar")
 def private_calendar():
     data = {}
     return render_template("private_calendar.html", **data)
-
-@app.route("/search/private_calendar_create")
-def private_calendar_create():
-    data = {}
-    return render_template("private_calendar_create.html", **data)
 
 """
 # What do we need....
