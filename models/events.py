0768f8b02dd93e4bcadd014a7e85b78e1f888139
jicruz96 <accounts@jicruz.com>
2022-04-16 15:18:15 -0500

    update app

--- models/events.py
+++ models/events.py
@@ -7,6 +7,6 @@ from .base import BaseModel, form_field
 @dataclass
 class Event(BaseModel):
     date: datetime.date = form_field("date")
-    time: datetime.time = form_field("datetime-local")
+    time: datetime.time = form_field("time")
     summary: str = form_field("textarea")
     description: str = form_field("textarea")
